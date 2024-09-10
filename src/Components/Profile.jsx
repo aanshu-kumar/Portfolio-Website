f7771e057b55f3dfb30ad37fdc3bfb9bed6a53be
Anshu kumar <anshu4125kumar@gmail.com>
2024-09-10 19:01:34 +0530

    Updated profile image

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,5 @@
 export default function Profile(){
-    return<div className="h-[100%] w-[100%] bg-cover bg-end rounded-lg bg-profile2">
+    return<div className="h-[100%] w-[100%] bg-cover bg-end rounded-lg bg-profile1 border-solid border-b-Light-orange">
         
     </div>
 }
