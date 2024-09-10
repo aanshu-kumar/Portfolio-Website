2054da054eabda6a4f2ec4e709ea175aa134b966
Anshu kumar <anshu4125kumar@gmail.com>
2024-09-10 19:15:32 +0530

    added socail links

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,7 +1,7 @@
 export default function Socials(){
     return<div className=" w-[95%] h-[100%] grid grid-rows-1 grid-cols-3  font-mono font-semibold m-auto">
-        <div className="m-auto"><span className="h-5 w-5 bg-LinkdinIcon"></span><p>Linkdin</p></div>
-        <div className="m-auto"><p>Github</p></div>
-        <div className="m-auto "><p>X<span className="font-light text-xs"> &#x28;formally Twitter&#x29;</span></p></div>
+        <div className="m-auto"><span className="h-5 w-5 bg-LinkdinIcon"></span><a href="https://www.linkedin.com/in/anshukumar01/" target="_blank"><p>Linkdin</p></a></div>
+        <div className="m-auto"><a href="https://github.com/aanshu-kumar" target="_blank"><p>Github</p></a></div>
+        <div className="m-auto "><a href="https://x.com/anshu__kr" target="_blank"><p>X<span className="font-light text-xs"> &#x28;formally Twitter&#x29;</span></p></a></div>
     </div>
 }
